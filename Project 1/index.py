d73a9626f186ba8d6b8a3e31d2a547261b56faf2
karolkecinski <58360925+karolkecinski@users.noreply.github.com>
2022-04-25 11:35:50 +0200

    Added dataInfo()

--- Project 1/index.py
+++ Project 1/index.py
@@ -3,5 +3,11 @@ import os
 
 #data = np.load(f'dataset{os.sep}training{os.sep}trainAccelerometer.npy')
 
-for file in os.listdir(f'dataset{os.sep}training{os.sep}'):
-    print(np.load(f'dataset{os.sep}training{os.sep}{file}'))
+def dataInfo():
+    for file in os.listdir(f'dataset{os.sep}training{os.sep}'):
+        print(f'{file:28} :   dimentions:   ', end = '')
+        print(str(np.load(f'dataset{os.sep}training{os.sep}{file}').ndim) + ',   length:  ', end = '')
+        print(len(np.load(f'dataset{os.sep}training{os.sep}{file}')))
+
+dataInfo()
+    
